c4247193c01e91f81c3441489d83ebc1f4cff554
Hephaestus1233 <thereadingmachine@gmail.com>
2022-03-27 16:08:31 -0700

    Now using Generics!

--- binarysearchtree/bst.go
+++ binarysearchtree/bst.go
@@ -1,40 +1,35 @@
 package binarysearchtree
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //BinaryNode Simple value as defined by user with two children
-type BinaryNode struct {
-	value interface{}
-	left  *BinaryNode
-	right *BinaryNode
+type BinaryNode[T any] struct {
+	value T
+	left, right  *BinaryNode[T]
 }
 
 //BinaryTree Simple Binary Tree, can define the comparator function (must be true for right value)
-type BinaryTree struct {
-	comparator func(interface{}, interface{}) bool
-	root *BinaryNode
+type BinaryTree[T any] struct {
+	comparator func(T, T) bool
+	root *BinaryNode[T]
 }
 
 //NewBinarySearchTree Creates a new Binary Search tree
-func NewBinarySearchTree(startValue interface{}, compareFunction func(interface{}, interface{}) bool) *BinaryTree {
-	nRootNode := new(BinaryNode)
+func NewBinarySearchTree[T any](startValue T, compareFunction func(T, T) bool) *BinaryTree[T] {
+	nRootNode := new(BinaryNode[T])
 	nRootNode.value = startValue
 	
-	return &BinaryTree{
+	return &BinaryTree[T]{
 		comparator: compareFunction,
 		root: nRootNode,
 	}
 }
 
 //Insert Inserts a new value into the tree, using the comparator function saved in the struct
-func (b *BinaryTree) Insert(value interface{}){
-	valType := fmt.Sprintf("%T", value)
-	rooType := fmt.Sprintf("%T", b.root.value)
-	if valType != rooType {
-		panic("Attempted to insert a " + valType + " into a " + rooType + " binary search tree.") //you handle it yourself baby, not my problem
-	}
-
-	var insertAfter *BinaryNode = b.root
+func (b *BinaryTree[T]) Insert(value T){
+	var insertAfter *BinaryNode[T] = b.root
 	for insertAfter != nil {
 		if b.comparator(value, insertAfter.value) { //if true, go to the right
 			if insertAfter.right == nil {
@@ -50,25 +45,39 @@ func (b *BinaryTree) Insert(value interface{}){
 	}
 
 	if b.comparator(value, insertAfter.value) {
-		insertAfter.right = new(BinaryNode)
+		insertAfter.right = new(BinaryNode[T])
 		insertAfter.right.value = value
 	}else{
-		insertAfter.left = new(BinaryNode)
+		insertAfter.left = new(BinaryNode[T])
 		insertAfter.left.value = value
 	}
 }
 
-//Remove removes the 
-func (b *BinaryTree) Remove(value interface{}){
+//Remove the value from location, and replace with best replacement
+func (b *BinaryTree[T]) Remove(value T, equal func(T, T) bool){
+	//Locate the node we want to remove
+	var removeNode *BinaryNode[T] = b.root
+	for removeNode != nil {
+		if equal(value, removeNode.value) { //if true, go to the right
+			removeNode = removeNode.right
+		}else{
+			removeNode = removeNode.left
+		}
+	}
 
+	//Locate the replacement
+	var replacement *BinaryNode[T] = removeNode
+	if replacement.right == nil {
+		
+	}
 }
 
 //InorderPrint Outputs the tree in order of seeing each node
-func (b BinaryTree) InorderPrint(){
+func (b BinaryTree[T]) InorderPrint(){
 	recurseInOrder(b.root)
 }
 
-func recurseInOrder(node *BinaryNode){
+func recurseInOrder[T any](node *BinaryNode[T]){
 	if node == nil {return}
 
 	recurseInOrder(node.left)
@@ -77,11 +86,11 @@ func recurseInOrder(node *BinaryNode){
 }
 
 //PreorderPrint Outputs the tree with the root node coming first
-func (b BinaryTree) PreorderPrint(){
+func (b BinaryTree[T]) PreorderPrint(){
 
 }
 
 //PostorderPrint Outputs the tree with the root node coming last
-func (b BinaryTree) PostorderPrint(){
+func (b BinaryTree[T]) PostorderPrint(){
 
 }
