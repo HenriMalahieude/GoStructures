cba8f6030a067da9e5858656af39febb3255c25e
HenriMalahieude <henrimalahieude@gmail.com>
2022-04-05 23:43:11 -0700

    Testing Linked Queue

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/HenriMalahieude/GoStructures/queue"
 	"github.com/HenriMalahieude/GoStructures/stack"
 )
 
@@ -25,6 +26,12 @@ func testStack() {
 
 	if tstack1.Peak() != 1 {
 		fmt.Println("Push/Peak (Case1): Not Passed, did not push/peak correctly")
+	}else{
+		total++
+	}
+
+	if tstack1.Empty() {
+		fmt.Println("Empty (Case2): Not passed")
 	}else{
 		total++
 	}
@@ -56,16 +63,71 @@ func testStack() {
 	tstack1.Pop()
 
 	if !tstack1.Empty(){
-		fmt.Println("Empty (Case2): Not Passed")
+		fmt.Println("Empty/Pop (Case1): Not Passed")
 	}else{
 		total++
 	}
 
-	fmt.Println("Test Passed on Stack: ", total, "/ 6")
+	fmt.Println("Test Passed on Stack: ", total, "/ 7")
 }
 
 func testQueue() {
+	var total int = 0;
+	tqueue1 := queue.NewLinkedQueue[int]()
 
+	if !tqueue1.Empty() {
+		fmt.Println("Empty (Case1): Not passed")
+	}else{
+		total++
+	}
+
+	tqueue1.Enqueue(1)
+
+	if tqueue1.Peak() != 1 {
+		fmt.Println("Enqueue/Peak (Case1): Not passed, incorrect peak value")
+	}else{
+		total++
+	}
+
+	if tqueue1.Empty() {
+		fmt.Println("Empty (Case2): Not passed")
+	}else{
+		total++
+	}
+
+	tqueue1.Enqueue(2)
+	tqueue1.Enqueue(3)
+
+	if tqueue1.Peak() != 1 {
+		fmt.Println("Enqueue/Peak (Case2): Not passed, incorrect peak value")
+	}else{
+		total++
+	}
+
+	val := tqueue1.Dequeue()
+
+	if val != 1 {
+		fmt.Println("Dequeue (Case1): Not passed, incorrect dequeue return")
+	}else{
+		total++
+	}
+
+	if tqueue1.Peak() != 2 {
+		fmt.Println("Dequeue (Case1.5): Not passed, next value incorrect")
+	}else{
+		total++
+	}
+
+	tqueue1.Dequeue()
+	tqueue1.Dequeue()
+
+	if !tqueue1.Empty() {
+		fmt.Println("Dequeue/Empty (Case1): not functioning properly")
+	}else{
+		total++
+	}
+
+	fmt.Println("Tests Passed on Queue:", total, "/ 7")
 }
 
 func testList() {
@@ -74,4 +136,5 @@ func testList() {
 
 func main() {
 	testStack()
+	testQueue()
 }
